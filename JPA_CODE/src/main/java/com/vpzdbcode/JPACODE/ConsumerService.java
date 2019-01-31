70b3b9e7912a74dbf9e4ead91a1ed8e9f71892fb
Vinod <vinodpawar8086@gmail.com>
2019-01-31 16:54:47 +0530

    Update ConsumerService.java

--- JPA_CODE/src/main/java/com/vpzdbcode/JPACODE/ConsumerService.java
+++ JPA_CODE/src/main/java/com/vpzdbcode/JPACODE/ConsumerService.java
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Service;
 public class ConsumerService
 {
 	@Autowired
-	DataSource dataSource;
-	@Autowired
 	ConsumerRepository repository;
 
 	@Transactional
